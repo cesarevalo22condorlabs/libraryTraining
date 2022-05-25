fa16fab21baef2e10557849d031d5336ff163c49
César Arévalo <cesar.arevalo@condorlabs.io>
2022-05-25 17:26:27 -0500

    se añade configuración de redis

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
+import responseTime from 'response-time'
 import schema from './Infraestructure/schemaGraph'
 import { connect } from './persistence/database'
+import redis from 'redis'
 
 const app = express()
 connect()
 
+const client = redis.createClient()
+
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+
+//header measure time for redis
+app.use(responseTime())
 
 //app.use routes
 app.use("/library", graphqlHTTP({
