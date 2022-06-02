12fd7f1ec4d0b0b8673c868a6c9ff84800cd322b
César Arévalo <cesar.arevalo@condorlabs.io>
2022-06-02 09:16:55 -0500

    implementación báseica de healt-middleeare

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ import express from 'express'
 import { graphqlHTTP } from 'express-graphql'
 import responseTime from 'response-time'
 import schema from './Infraestructure/schemaGraph.js'
+import {healthMonitor} from '@condor-labs/health-middleware'
 
 require('dotenv').config();
 
 const app = express();
-
+healthMonitor(app);
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
